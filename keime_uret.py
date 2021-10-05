43d0ed140642926256e2c1c65ec460af03f75284
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-10-06 00:16:47 +0300

    kod yazilmaya basladi

--- keime_uret.py
+++ keime_uret.py
@@ -1,1 +1,15 @@
-girdi = input("kullanılacak kelimeleri, aralarına virgül koyarak ekleyin.\n")
+with open("TDK_Sozluk_Kelime_Listesi.txt", "r", encoding="UTF-8") as dosya:
+    satir = dosya.read()
+
+    alfabe = "abcçdefgğhıijklmnoöprsştuüvyz"
+    girdi = input("Kelime üretiminde kullanilacak harfleri, aralarinda bosluk olmadan ekleyin. Ornegin: trky.\nKullanilabilecek harfler : abcçdefgğhıijklmnoöprsştuüvyz \n:").lower()
+
+    for harf in girdi:
+        if harf not in alfabe:
+            print("UYARI: Yazilan metin icerisinde, Turkce alfabede olmayan karakter bulunduğu icin, isleme devam edilemiyor!")
+            break
+
+        else:
+            for harf in girdi:
+                if harf in satir:
+                    print(satir)
