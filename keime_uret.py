fee50f708587610b71817e9daf7c806e3ff89417
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2021-10-16 21:59:26 +0300

    girdi harfleri kelimede var
    Kullanicinin yazdigi harfler, sozluk satirlari (liste elemanlari) icerisinde varsa, satiri (liste elemanini) ekrana yazdiriyor.

--- keime_uret.py
+++ keime_uret.py
@@ -1,15 +1,21 @@
 with open("TDK_Sozluk_Kelime_Listesi.txt", "r", encoding="UTF-8") as dosya:
-    satir = dosya.read()
+    liste = dosya.readlines()
+#    print(satir)
 
     alfabe = "abcçdefgğhıijklmnoöprsştuüvyz"
     girdi = input("Kelime üretiminde kullanilacak harfleri, aralarinda bosluk olmadan ekleyin. Ornegin: trky.\nKullanilabilecek harfler : abcçdefgğhıijklmnoöprsştuüvyz \n:").lower()
 
     for harf in girdi:
         if harf not in alfabe:
-            print("UYARI: Yazilan metin icerisinde, Turkce alfabede olmayan karakter bulunduğu icin, isleme devam edilemiyor!")
+            print("UYARI: izin verilen harflerin disinda bir veya daha fazla harf yazdiginiz icin, isleme devam edilemiyor!")
             break
-
-        else:
+        
+        puan = 0
+        for kelime in liste:
             for harf in girdi:
-                if harf in satir:
-                    print(satir)
+                if harf in kelime[:-1]:
+                    puan += 1
+            if puan >= len(girdi):
+                print(kelime[:-1])
+                
+            puan = 0
