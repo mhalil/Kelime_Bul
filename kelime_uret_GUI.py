dc79084bf99ee2b26fd2cb5e78547d1627318414
mhalil <halil.mustafa@gmail.com>
2022-05-08 17:30:42 +0300

    buton yerleşimi  OK arama, temizleme  fonk. OK

--- kelime_uret_GUI.py
+++ kelime_uret_GUI.py
@@ -4,7 +4,9 @@
 from faulthandler import disable
 from pickle import FALSE
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import DISABLED, END, messagebox
+from tkinter.font import NORMAL
+from turtle import right
 
 #Fonksiyonlar
 
@@ -13,18 +15,31 @@ def ara():
         liste = dosya.readlines()
         alfabe = "abcçdefgğhıijklmnoöprsştuüvyz"
 
-        for harf in girdi:
-            if harf not in alfabe:
-                messagebox.showerror("Hatalı Karakter!","izin verilen harflerin disinda bir veya daha fazla harf yazdiginiz icin, isleme devam edilemiyor!")
- 
+        for harf in girdi.get():
+            if harf not in alfabe or girdi.get() == "Lütfen geçerli karakter yazın":  # Hatayı tek sefer almam lazım, geçersiz karakter sayısı kadar değil!
+                print("Hata!, Geçersiz Karakterler")
+                messagebox.showerror("Hatalı Karakter!","izin verilen harflerin disinda bir veya daha fazla harf yazdiginiz icin, isleme devam edilemiyor!\nKullanılabilir karakterler şunlardır:\nabcçdefgğhıijklmnoöprsştuüvyz")
+
+            puan = 0
+            for kelime in liste:
+                for harf in girdi.get():
+                    if harf in kelime[:-1]:
+                        puan += 1
+                if puan >= len(girdi.get()):
+                    cikti.insert(END,kelime)
+                    
+                puan = 0
 
 
 def kaydet():
     pass
 
+def temizle():
+    girdi.bind = girdi.delete(0,END)
+    cikti.delete("1.0","end")
 
 pencere = tk.Tk()
-pencere.geometry("300x400+600+300")
+pencere.geometry("300x450+600+300")
 pencere.resizable("FALSE", "FALSE")
 pencere.title("Kelime Üret")
 
@@ -37,39 +52,62 @@ aciklama = tk.Label(pencere, text = "Bulmak istediğiniz kelimeye ait \nHarfleri
 aciklama.place(x=20, y=10)
 
 girdi = tk.Entry(pencere, width=34)
+girdi.insert(0,"Lütfen geçerli karakter yazın")
+
 girdi.place(x=10, y=80)
 
 cikti = tk.Text(pencere,
-                width=34,
-                height=14,
-                state="disabled")
+                width=31,
+                height=15,
+                state="normal")
 cikti.place(x=10, y=110)
+
+kaydirma_cubugu = tk.Scrollbar(pencere,
+                                width=15, 
+                                command=cikti.yview,
+                                orient="vertical")
+kaydirma_cubugu.place(x=270, y=110)
+
+
+
+btn_genisligi = 12
 
 btn_ara = tk.Button(pencere,
                     text = "Ara",
-                    width=7,
+                    width = btn_genisligi,
                     # bg = "Yellow",
                     # fg = "red",
                     font = "Tahoma 10 bold",
+                    # state=DISABLED,
                     command= ara)
-btn_ara.place(x=10, y=360)
+btn_ara.place(x=10, y=380)
+
+btn_temizle = tk.Button(pencere,
+                    text = "Temizle",
+                    width = btn_genisligi,
+                    # bg = "Yellow",
+                    # fg = "red",
+                    font = "Tahoma 10 bold",
+                    command= temizle)
+btn_temizle.place(x=150, y=380)
+
 
 btn_kaydet = tk.Button(pencere,
                     text = "Kaydet",
-                    width=7,
+                    width = btn_genisligi,
                     # bg = "Yellow",
                     # fg = "red",
                     font = "Tahoma 10 bold",
                     command= kaydet)
-btn_kaydet.place(x=105, y=360)
+btn_kaydet.place(x=10, y=415)
 
 btn_kapat = tk.Button(pencere,
                     text = "Kapat",
-                    width=7,
+                    width = btn_genisligi,
                     # bg = "Yellow",
                     # fg = "red",
                     font = "Tahoma 10 bold",
                     command=quit)
-btn_kapat.place(x=200, y=360)
+btn_kapat.place(x=150, y=415)
 
 pencere.mainloop()
