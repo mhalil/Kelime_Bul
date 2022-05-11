35e113533b6f4aebbaff4aebfe56eb2a48ff406e
mhalil <halil.mustafa@gmail.com>
2022-05-11 21:14:00 +0300

    arabirim renklendirildi, sozluk guncellendi

--- kelime_bul_GUI.py
+++ kelime_bul_GUI.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from faulthandler import disable
+from sre_parse import State
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import END, messagebox
@@ -11,7 +12,7 @@ def ara():
     
     cikti.delete("1.0",tk.END)
 
-    with open("TDK_Sozluk_Kelime_Listesi.txt", "r", encoding="UTF-8") as dosya:
+    with open("Kelime_Listesi.txt", "r", encoding="UTF-8") as dosya:
         liste = dosya.readlines()
         alfabe = "abcçdefgğhıijklmnoöprsştuüvyz"     
 
@@ -47,37 +48,48 @@ def ara():
 
 def kaydet():
     with open("sonuc.txt", "w", encoding="UTF-8") as dosya:
-        dosya.write("'" + girdi.get() + "' Karakterlerini barındıran kelime araması sonucu " + cikti.index("end")[:-2] + " adet sonuç bulunmuştur.\nKelime(ler) Aşağıda listelenmiştir.\n\n" + cikti.get("1.0","end"))
+        dosya.write("'" + girdi.get() + "' Karakterlerini barındıran kelime araması sonucu " + cikti.index("end")[:-2] + " adet sonuç bulunmuştur.\nArama sonucu bulunan kelimeler, aşağıda listelenmiştir.\n\n" + cikti.get("1.0","end"))
 
 def temizle():
     girdi.delete(0, tk.END)
     cikti.delete("1.0",tk.END)
     sonuc["text"] = ""
 
+arkaplan_rengi = "#17a589"
+arkaplan_rengi_metin = "#d1f2eb"
+arkaplan_rengi_buton = "#17a589"
+
 pencere = tk.Tk()
 pencere.geometry("300x450+600+300")
 pencere.resizable("FALSE", "FALSE")
 pencere.title("Kelime Bul ve Listele")
+pencere.configure(bg = arkaplan_rengi)
 
 # Arayüz Unsurlarının (Widget) Yerleşimi
-aciklama = tk.Label(pencere, text = "Bulmak istediğiniz kelimeye ait \nHarfleri, aralarında boşluk bırakmadan \naşağıdaki kutuya  yazın;",
-                    fg = "blue",
-                    font = "Tahoma 10")
-aciklama.place(x=20, y=10)
+aciklama = tk.Label(pencere, text = "Aradığınız kelimeye ait harfleri,\naralarında boşluk bırakmadan \naşağıdaki kutuya  yazın. Ör. tryk",
+                    fg = "white",
+                    bg = arkaplan_rengi,
+                    font = "Verdana 11",
+                    width=32)
+aciklama.place(x=4, y=10)
 
-girdi = tk.Entry(pencere, width=34)
-# girdi.insert(0,"Lütfen geçerli karakter yazın")
+girdi = tk.Entry(pencere, 
+                width=34,
+                bg = arkaplan_rengi_metin)
 girdi.place(x=10, y=80)
 
 cikti = tk.Text(pencere,
                 width=34,
                 height=14,
-                state="normal")
+                state="normal",
+                bg = arkaplan_rengi_metin)
 cikti.place(x=10, y=110)
 
 sonuc = tk.Label(pencere,
-                text="",
-                fg = "blue")
+                text = "",
+                font = "Verdana 10 bold",
+                fg = "white",
+                bg = arkaplan_rengi)
 sonuc.place(x=10, y= 357)                
 
 btn_genisligi = 12
@@ -85,14 +97,18 @@ btn_genisligi = 12
 btn_ara = tk.Button(pencere,
                     text = "Ara",
                     width = btn_genisligi,
-                    font = "Tahoma 10 bold",
+                    font = "Verdana 10 bold",
+                    bg = arkaplan_rengi_buton,
+                    fg = "white",
                     command= ara)
 btn_ara.place(x=10, y=380)
 
 btn_temizle = tk.Button(pencere,
                     text = "Temizle",
                     width = btn_genisligi,
-                    font = "Tahoma 10 bold",
+                    font = "Verdana 10 bold",
+                    bg = arkaplan_rengi_buton,
+                    fg = "white",
                     command= temizle)
 btn_temizle.place(x=150, y=380)
 
@@ -100,14 +116,18 @@ btn_temizle.place(x=150, y=380)
 btn_kaydet = tk.Button(pencere,
                     text = "Kaydet",
                     width = btn_genisligi,
-                    font = "Tahoma 10 bold",
+                    font = "Verdana 10 bold",
+                    bg = arkaplan_rengi_buton,
+                    fg = "white",
                     command= kaydet)
 btn_kaydet.place(x=10, y=415)
 
 btn_kapat = tk.Button(pencere,
                     text = "Kapat",
                     width = btn_genisligi,
-                    font = "Tahoma 10 bold",
+                    font = "Verdana 10 bold",
+                    bg = "#cd6155",
+                    fg = "white",
                     command=quit)
 btn_kapat.place(x=150, y=415)
 
