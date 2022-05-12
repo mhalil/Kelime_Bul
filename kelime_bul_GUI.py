c7f8d02c418567fca0d73c7676e31d0aa0e7a677
mhalil <halil.mustafa@gmail.com>
2022-05-12 23:44:27 +0300

    menu kisayol tusu duzenlendi

--- kelime_bul_GUI.py
+++ kelime_bul_GUI.py
@@ -89,16 +89,13 @@ pencere.config(menu=menu_cubugu) #menümüzü oluşturduk
 
 dosya_menusu = tk.Menu(menu_cubugu)
 menu_cubugu.add_cascade(label="Dosya", menu=dosya_menusu)
-dosya_menusu.add_command(label="Ara", command=ara, accelerator= "Ctrl+1")
-dosya_menusu.add_command(label="Temizle", command=temizle, accelerator= "Ctrl+2")
-dosya_menusu.add_command(label="Kaydet", command=kaydet, accelerator= "Ctrl+3")
+dosya_menusu.add_command(label="Ara", command=ara)
+dosya_menusu.add_command(label="Temizle", command=temizle)
+dosya_menusu.add_command(label="Kaydet", command=kaydet)
 dosya_menusu.add_command(label="Kapat", command=pencere.quit, accelerator= "Ctrl+Q")
 
-pencere.bind("<Control-q>", exit)
-pencere.bind("<Control-Q>", exit)
-pencere.bind("<Control-1>", ara)
-pencere.bind("<Control-2>", temizle)
-pencere.bind("<Control-3>", kaydet)
+pencere.bind_all("<Control-q>", exit)
+pencere.bind_all("<Control-Q>", exit)
 
 hakkinda_menusu = tk.Menu(menu_cubugu, tearoff=0)
 menu_cubugu.add_cascade(label="Hakkında", menu=hakkinda_menusu)
